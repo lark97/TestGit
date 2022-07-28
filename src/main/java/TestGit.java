dc324b43d5728754afb77ad12083986082c48918
lark <1119676546@qq.com>
2022-07-29 00:38:13 +0800

    init "初始化"

---
+++ src/main/java/TestGit.java
@@ -0,0 +1,11 @@
+/**
+ * @program: untitled
+ * @description: 测试类
+ * @author: lark
+ * @create: 2022-07-29 00:29
+ **/
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+    }
+}
