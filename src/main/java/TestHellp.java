74d4c03bfe7ce76c65eeff5b430d49bf798b4c38
lark <1119676546@qq.com>
2022-07-29 01:14:17 +0800

    test02

--- src/main/java/TestHellp.java
+++ src/main/java/TestHellp.java
@@ -7,5 +7,9 @@
 public class TestHellp {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        for(int i = 0;i < 5;i++){
+            System.out.println("thanks");
+        }
     }
 }
